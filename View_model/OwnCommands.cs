7c2d2e5de71f92925af71eb407f256507f1eb109
Volodymyr <klymenkowolodymyr@gmail.com>
2017-11-14 19:47:09 +0200

    Add files via upload

---
+++ View_model/OwnCommands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Task4
+{
+    public class RelayCommand : ICommand
+    {
+        private readonly Action<object> execute;
+        private readonly Predicate<object> canExecute;
+
+
+        public RelayCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+
+        public void Execute(object parameters)
+        {
+            this.execute(parameters);
+        }
+    }
+}
---
+++ View_model/ViewModel_Main.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+
+namespace Task4
+{
+    public class ViewModel_Main : DependencyObject
+    {
+        public ObservableCollection<Dish> SelectedDishes { get; set; }
+
+        public ObservableCollection<Dish> Dishes
+        {
+            get { return ((ObservableCollection<Dish>)GetValue(DishesCollectionProperty)); }
+            set { SetValue(DishesCollectionProperty, value); }
+        }
+        public static readonly DependencyProperty DishesCollectionProperty =
+            DependencyProperty.Register("Dishes", typeof(ObservableCollection<Dish>), typeof(ViewModel_Main));
+
+
+        public float TotalPrice
+        {
+            get { return (float)GetValue(TotalPriceProperty); }
+            set { SetValue(TotalPriceProperty, value); }
+        }
+        public static readonly DependencyProperty TotalPriceProperty =
+            DependencyProperty.Register("TotalPrice", typeof(float), typeof(ViewModel_Main));
+
+
+        private RelayCommand selectDishCmd;
+        public RelayCommand SelectDishCmd
+        {
+            get
+            {
+                return selectDishCmd ??
+                    (
+                        selectDishCmd = new RelayCommand(
+                        (obj) =>{
+                            Dish dish = new Dish((Dishes[(int)obj].Name), (Dishes[(int)obj].Price), (Dishes[(int)obj].Detail));
+
+                            TotalPrice += dish.Price;
+
+                            SelectedDishes.Add(dish);
+                        },
+                    
+                        (obj)=> 
+                        {
+                            if (obj == null || (int)obj < 0 || (int)obj >= Dishes.Count)
+                                return false;
+                            return true;
+                        })
+                    );
+            }
+        }
+
+        private RelayCommand resetDishCmd;
+        public RelayCommand ResetDishCmd
+        {
+            get
+            {
+                return resetDishCmd ??
+                    (
+                        resetDishCmd = new RelayCommand((obj) =>
+                        {
+                            while(SelectedDishes.Count != 0)
+                            {
+                                SelectedDishes.RemoveAt(0);
+                            }
+                            TotalPrice = 0;
+                        },
+                        (obj) => { return SelectedDishes.Count > 0; }
+                        
+                    ));
+            }
+        }
+
+        private RelayCommand makeOrderCommand;
+        public RelayCommand MakeOrderCommand
+        {
+            get
+            {
+                return makeOrderCommand ??
+                    (
+                        makeOrderCommand = new RelayCommand(
+                            (obj) =>
+                            {
+                                Dish.WriteOrder(SelectedDishes);
+
+                                MessageBox.Show("Thank`s for order !\n Your order is written in file for cooker)");
+
+                                while (SelectedDishes.Count != 0)
+                                {
+                                    SelectedDishes.RemoveAt(0);
+                                }
+                                TotalPrice = 0;
+                            },
+                            (obj) => { return SelectedDishes.Count > 0; }
+                    ));
+            }
+        }
+
+        public ViewModel_Main()
+        {
+            Dishes = Dish.ReadDishes();
+            SelectedDishes = new ObservableCollection<Dish>();
+            TotalPrice = 0;
+
+        }
+
+    }
+}
